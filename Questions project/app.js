5f4cd0d43ec0ca4059c9092884dc12fef1c45416
Acha Rha'ah <acharhaah2003@gmail.com>
2022-02-26 20:28:47 +0100

    done with html and started JS

--- Questions project/app.js
+++ Questions project/app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+
+const plusBtn = document.querySelector(".plus-icon");
+const minusBtn = document.querySelector(".minus-icon");
+const questionsText = document.querySelector(".question-text");
+
+plusBtn.addEventListener("click", function(){
+    
+})
